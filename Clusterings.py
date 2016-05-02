d35eef3eef14085e3d2726913c7c7fa003bab62d
tianxiao zhang <tzhang46@illinois.edu>
2016-05-02 17:38:58 -0500

    update clsuter result files and add id2ptn

--- Clusterings.py
+++ Clusterings.py
@@ -5,6 +5,10 @@ from kmeans import kmeans
 from PreprocessData import readBusinessata
 from kmeans import Point
 
+
+finput = open('Business2Index.txt','r')
+Business2Index = np.loadtxt(finput,dtype='str')
+
 def buildLabels(est,X):
     cluster_dict = {}
     id = 0
@@ -14,18 +18,27 @@ def buildLabels(est,X):
         id += 1
 
     labels = []
-    for point in X:
-        labels.append(cluster_dict[point])
-    return np.array(labels)
+    id2point = []
+    for i in xrange(len(X)):
+        point = X[i]
+        point_id = Business2Index[i]
+        labels.append(point_id+' '+str(cluster_dict[point]))
+        id2point.append((point_id,point.coords[1],point.coords[0]))
 
 
+    return (labels,id2point)
 
+
+def buildLabels2(lables,X):
+    result = []
+    for i in xrange(len(X)):
+        point_id = Business2Index[i]
+        result.append(point_id+' '+str(lables[i]))
+    return result
 
 def prepareX():
     X = []
     business_dict = readBusinessata('/Users/ztx/Downloads/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json')
-    finput = open('Business2Index.txt','r')
-    Business2Index = np.loadtxt(finput,dtype='str')
     finput.close()
     for id in Business2Index:
         point =  Point(business_dict[id])
@@ -50,14 +63,17 @@ def main():
 
     est = kmeans(X,k,opt_cutoff)
 
-    cluster_label0 = buildLabels(est,X)
+    (cluster_label0,id2point) = buildLabels(est,X)
     cluster_label1 = SpectralClustering(k).fit_predict(mat)
+    cluster_label1 = buildLabels2(cluster_label1,X)
     cluster_label2 = DBSCAN(min_samples=min_samples).fit_predict(mat)
+    cluster_label2 = buildLabels2(cluster_label2,X)
 
+    np.savetxt('Keans.txt', cluster_label0, fmt='%s', newline='\n', header='', footer='', comments='# ')
+    np.savetxt('SpectralClustering.txt', cluster_label1, fmt='%s', newline='\n', header='', footer='', comments='# ')
+    np.savetxt('DBSCAN.txt', cluster_label2, fmt='%s', newline='\n', header='', footer='', comments='# ')
 
-    np.savetxt('Keans.txt', cluster_label0, fmt='%d', newline='\n', header='', footer='', comments='# ')
-    np.savetxt('SpectralClustering.txt', cluster_label1, fmt='%d', newline='\n', header='', footer='', comments='# ')
-    np.savetxt('DBSCAN.txt', cluster_label2, fmt='%d', newline='\n', header='', footer='', comments='# ')
+    np.savetxt('ID2Point.txt', id2point, fmt=["%s",]*3, newline='\n')
 
 if __name__ == "__main__":
     main()
